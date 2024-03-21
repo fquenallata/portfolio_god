20de4269de63464999dfdb44f55b1c6c87384635
Quena <quenallatafranco@gmail.com>
2024-03-20 22:51:56 -0300

    añadiendo themetoggle

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 import ChangeIdiom from '../../components/Buttons/ChangeIdiom';
+import ThemeToggle from '../../components/Buttons/ThemeToggle';
 import './Home.scss';
 
 const Home = ({ t }) => {
 	return (
 		<section id='Home'>
+			<ThemeToggle />
 			<ChangeIdiom text={t('idiom')} />
 		</section>
 	);
