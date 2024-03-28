381d4ad2170c0ddaa54bb4cf660d2ad81ee6ca60
Franco Quenallata <quenallatafranco@gmail.com>
2024-03-28 19:47:28 -0300

    buenos cambios :)

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import SocialButtons from '../Buttons/SocialButtons';
 import LetraFSVG from '../icons/LetraF';
 import './Nav.scss';
 
 const Nav = ({ t }) => {
+	const [selectedLink, setSelectedLink] = useState('home');
+
+	const handleLinkClick = (link) => {
+		setSelectedLink(link);
+	};
+
 	return (
 		<section id='Nav'>
 			<div className='logoName'>
@@ -13,25 +20,41 @@ const Nav = ({ t }) => {
 			</div>
 			<ul>
 				<li>
-					<a href='#home'>
+					<a
+						href='#home'
+						className={selectedLink === 'home' ? 'selected' : ''}
+						onClick={() => handleLinkClick('home')}
+					>
 						<p>{`•    `}</p>
 						{`${t('nav.Home')}`}
 					</a>
 				</li>
 				<li>
-					<a href='#about'>
+					<a
+						href='#about'
+						className={selectedLink === 'about' ? 'selected' : ''}
+						onClick={() => handleLinkClick('about')}
+					>
 						<p>{`•    `}</p>
 						{t('nav.About')}
 					</a>
 				</li>
 				<li>
-					<a href='#portfolio'>
+					<a
+						href='#portfolio'
+						className={selectedLink === 'portfolio' ? 'selected' : ''}
+						onClick={() => handleLinkClick('portfolio')}
+					>
 						<p>{`•    `}</p>
 						{t('nav.Portfolio')}
 					</a>
 				</li>
 				<li>
-					<a href='#skills'>
+					<a
+						href='#skills'
+						className={selectedLink === 'skills' ? 'selected' : ''}
+						onClick={() => handleLinkClick('skills')}
+					>
 						<p>{`•    `}</p>
 						{t('nav.Skills')}
 					</a>
